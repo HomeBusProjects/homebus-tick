0afc80be2ff359a35739e2bccb38b6c8825c70a5
John Romkey <romkey@romkey.com>
2018-05-29 08:37:28 -0700

    output is now optional

--- tick.rb
+++ tick.rb
@@ -10,6 +10,8 @@ Dotenv.load '.env.provision'
 
 require 'homebus'
 
+opts = []
+
 mqtt = { host: ENV['MQTT_HOSTNAME'],
          port: ENV['MQTT_PORT'],
          username: ENV['MQTT_USERNAME'],
@@ -18,7 +20,8 @@ mqtt = { host: ENV['MQTT_HOSTNAME'],
 
 uuid = ENV['UUID']
 
-pp mqtt
+pp mqtt if opts[:debug]
+
 
 if mqtt[:host].nil?
   puts 'host is nil'
@@ -44,7 +47,7 @@ if mqtt[:host].nil?
     abort 'MQTT provisioning failed'
   end
 
-  pp mqtt
+  pp mqtt if opts[:debug]
 
   uuid = mqtt[:uuid]
   mqtt.delete :uuid
@@ -62,16 +65,15 @@ loop do
     weekday: Time.now.wday,
     hour: Time.now.hour,
     minute: Time.now.min,
-    second: Time.now.sec
+    second: Time.now.sec,
     timezone_code: Time.now.zone,
     timezone_offset: Time.now.utc_offset
   }
 
-  puts 'about to tick'
-  pp tick_msg
+  pp tick_msg if opts[:verbose]
   
   client.publish('tick', tick_msg.to_json, true)
-  puts 'ticked'
+  puts 'ticked' if opts[:verbose]
 
   sleep 1            
 end
