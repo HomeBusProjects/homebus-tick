3afb7ee047ec16c009ab7319259a123dfaaa48ee
John Romkey <58883+romkey@users.noreply.github.com>
2021-01-18 10:10:16 -0800

    bumped Ruby version to 2.6.6
    updated gems to get updated Homebus library
    
    now ticks and publishes clock

--- homebus-tick.rb
+++ homebus-tick.rb
@@ -2,98 +2,11 @@
 
 require 'mqtt'
 require 'json'
-
-require 'homebus'
-require 'homebus_app'
-require 'homebus_app_options'
-
-require 'pp'
-
-class TickHomeBusAppOptions < HomeBusAppOptions
-  def app_options(op)
-  end
-
-  def banner
-    'HomeBus Ticker'
-  end
-
-  def version
-    '0.9.0'
-  end
-
-  def name
-    'homebus-tick'
-  end
-end
-
-class TickHomeBusApp < HomeBusApp
-  DDC = 'org.homebus.experimental.tick'
-
-  def setup!
-  end
-
-  def work!
-    t = Time.now
-
-    tick_msg = {
-      epoch: t.to_i,
-      year: t.year,
-      month: t.month,
-      month_day: t.mday,
-      weekday: t.wday,
-      hour: t.hour,
-      minute: t.min,
-      second: t.sec,
-      timezone_code: t.zone,
-      timezone_offset: t.utc_offset
-    }
-
-    if @options[:verbose]
-      pp tick_msg
-    end
-  
-    publish! DDC, tick_msg
-
-    sleep 1
-  end
-
-  def manufacturer
-    'HomeBus'
-  end
-
-  def model
-    'Basic'
-  end
-
-  def friendly_name
-    'Ticker'
-  end
-
-  def friendly_location
-    'HomeBus Core'
-  end
-
-  def serial_number
-    ''
-  end
-
-  def pin
-    ''
-  end
-
-  def devices
-    [ {
-        friendly_name: 'System Ticker',
-        friendly_location: 'The Core',
-        update_frequency: 1000,
-        accuracy: 10,
-        precision: 100,
-        wo_topics: [ DDC ],
-        ro_topics: [],
-        rw_topics: []
-      } ]
-  end
-end
+require './homebus'
+require './homebus_app'
+require './homebus_app_options'
+require './app'
+require './options'
 
 tick_app_options = TickHomeBusAppOptions.new
 
